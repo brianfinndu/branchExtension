599244173cf0caa2a11dca53653c566523142867
Brian Du <brian.finn.du@gmail.com>
2024-11-21 12:05:10 -0800

    Set up files for individual dev demo assignment.

--- branchTest.js
+++ branchTest.js
@@ -1,1 +1,21 @@
-console.log('print a test string')
+function postKey() {
+  let devKey = prompt("Please enter an ID.");
+  let devName = prompt("Please enter your name.");
+
+  let obj = {};
+  obj[devKey] = devName;
+
+  chrome.storage.sync
+    .set(obj)
+    .then(() => {
+      console.log("Key value pair created.");
+    })
+    .then(() => {
+      console.log(devKey);
+      chrome.storage.sync.get([devKey.toString()]).then((result) => {
+        console.log("Result is " + JSON.stringify(result));
+      });
+    });
+}
+
+postKey();
